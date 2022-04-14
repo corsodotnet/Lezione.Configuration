3a19746f941d6f08d9e1b0c5a662145233cb45f7
Bruno Ferreira Rocha <bruno_ferreira@hotmail.it>
2022-04-14 10:28:54 +0200

    Add project files.

---
+++ Lezione.Configuration/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Lezione.Configuration
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // build a config object, using env vars or/and JSON provider
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            Settings settings = config.GetRequiredSection("settings").Get<Settings>();
+
+            Console.WriteLine(settings.name);
+
+        }
+    }
+
+    public class Settings
+    {
+        public string name { get; set; }
+    }
+}
